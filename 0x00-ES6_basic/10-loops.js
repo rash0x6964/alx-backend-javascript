16c5b9abbc4a52b0a6d18fec9c67f85e0cd22c8d
rash0x6964 <rash0x6964@gmail.com>
2024-07-22 21:50:46 +0100

    task 10: For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const res = [];
+
+  for (const value of array) {
+    res.push(appendString + value);
+  }
+
+  return res;
+}
