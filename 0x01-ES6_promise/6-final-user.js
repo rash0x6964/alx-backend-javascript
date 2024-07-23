cfc8cf0a02275417495d9abb15053619a579a7d6
rash0x6964 <rash0x6964@gmail.com>
2024-07-23 13:30:39 +0100

    task 6: Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((result) => {
+    result.map((obj) => ({
+      status: obj.status,
+      value: obj.status === 'fulfilled' ? obj.value : obj.reason,
+    }));
+  });
+}
