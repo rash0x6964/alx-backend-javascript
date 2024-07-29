5472fd8ceb59ac3490501c48882cf817145ae522
rash0x6964 <rash0x6964@gmail.com>
2024-07-29 21:49:50 +0100

    task 10: More map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,4 @@
-export default function groceriesList = () => {
+export default function groceriesList() {
   const map = new Map();
   map.set('Apples', 10);
   map.set('Tomatoes', 10);
@@ -6,4 +6,4 @@ export default function groceriesList = () => {
   map.set('Rice', 1);
   map.set('Banana', 5);
   return map;
-};
+}
