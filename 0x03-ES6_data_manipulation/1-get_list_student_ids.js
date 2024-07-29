138f47c9da528dd20d5283e96455a2d7d8d5612d
rash0x6964 <rash0x6964@gmail.com>
2024-07-29 21:05:36 +0100

    task 1: More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,6 @@
+export default function getListStudentIds(listStudents) {
+  let listStudentIds = [];
+  if (!(listStudents instanceof Array)) return listStudentIds;
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+}
