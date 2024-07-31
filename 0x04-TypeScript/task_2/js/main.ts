f2b4efd36b6cb0656ff6bc81e23fb0b559a39fd2
rash0x6964 <rash0x6964@gmail.com>
2024-07-31 12:43:34 +0100

    task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,8 +45,29 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-// Test
+// Task 5
 
 // console.log(createEmployee(200));
 // console.log(createEmployee(1000));
 // console.log(createEmployee('$500'));
+
+// -------------------------------------------------------------------
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
+
+// Task 6
+
+// console.log(executeWork(createEmployee(200)));
+// console.log(executeWork(createEmployee(1000)));
+
+// -------------------------------------------------------------------
