0495a77138a3a80c57fb38c6db86350880709ccc
rash0x6964 <rash0x6964@gmail.com>
2024-07-31 12:31:01 +0100

    task 1/2/3/4

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,92 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+// Task 1
+
+// const teacher3: Teacher = {
+//   firstName: 'John',
+//   fullTimeEmployee: false,
+//   lastName: 'Doe',
+//   location: 'London',
+//   contract: false,
+// };
+
+// console.log(teacher3);
+
+// ------------------------------------------------------------
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Task 2
+
+// const director1: Directors = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   location: 'London',
+//   fullTimeEmployee: true,
+//   numberOfReports: 17,
+// };
+
+// console.log(director1);
+
+// ------------------------------------------------------------
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Task 3
+
+// console.log(printTeacher('John', 'Doe'));
+
+// ------------------------------------------------------------
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Task 4
+
+// const student = new StudentClass('John', 'Doe');
+// console.log(student.displayName());
+// console.log(student.workOnHomework());
+
+// ------------------------------------------------------------
