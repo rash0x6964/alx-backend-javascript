d5b75981145708d0e4b6e378311ff9d408bd5a68
rash0x6964 <rash0x6964@gmail.com>
2024-09-17 17:50:48 +0100

    fix issue in task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,6 +3,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
   process.exit();
 });
