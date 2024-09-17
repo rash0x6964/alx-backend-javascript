de64d36e95af441dcc678fc8c755e31fa4556905
rash0x6964 <rash0x6964@gmail.com>
2024-09-17 12:05:43 +0100

    task 3: Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+      if (lines.length <= 1) {
+        console.log('Number of students: 0');
+        resolve();
+        return;
+      }
+
+      const students = lines.slice(1);
+      console.log(`Number of students: ${students.length}`);
+
+      const fieldGroups = {};
+      students.forEach((student) => {
+        const studentData = student.split(',');
+
+        const field = studentData[studentData.length - 1];
+        const firstName = studentData[0];
+
+        if (!fieldGroups[field]) {
+          fieldGroups[field] = [];
+        }
+        fieldGroups[field].push(firstName);
+      });
+
+      for (const [field, names] of Object.entries(fieldGroups)) {
+        console.log(
+          `Number of students in ${field}: ${names.length}. List: ${names.join(
+            ', ',
+          )}`,
+        );
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
